0e5e179365c35317f45ac21407206c79e398002a
test <natitshow@gmail.com>
2020-04-09 00:18:29 +0300

    lecture 9.3

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -4,6 +4,7 @@ import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
 import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import biz.futureware.mantis.rpc.soap.client.ProjectData;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
@@ -11,6 +12,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.Set;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class SoapTests extends TestBase{
 
@@ -21,5 +24,14 @@ public class SoapTests extends TestBase{
         for (Project project : projects) {
             System.out.println(project.getName());
         }
+    }
+
+    @Test
+    public void yestCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+        Set<Project> project = applicationManager.soap().getProjects();
+        Issue issue = new Issue().withSummary("Test issue").withDescription("test issue description")
+                .withProject(project.iterator().next());
+      Issue created =  applicationManager.soap().addIssue(issue);
+      assertEquals(issue.getSummary(), created.getSummary());
     }
 }
